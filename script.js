2711adb34b68459cdc6332de1887d25c3eade762
lebakataraknathreddy123-afk <lebakataraknathreddy123@gmail.com>
2026-06-05 16:57:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+//Task 1
+studentName = "Naveen"
+console.log(studentName)
+studentAge = 18
+console.log(studentAge)
+studentCourse = "MERN Stack"
+console.log(studentCourse)
+
+//Task 2
+ varcompany = "Stackly"
+ console.log(varcompany)
+ letsalary=20000
+ console.log(letsalary)
+ constcountry = "india"
+ console.log(constcountry)
+ company ="Google"
+ console.log(company)
+ salary = 30000
+ console.log(salary)
+
+ //Task 3
+let name = prompt(" what is your name?")
+console.log("Welcome " + name) 
+
+//Task 4
+
+ let result= confirm("Do you know javaScript?")
+ console.log(result)
+
+//Task 5
+
+studentName = prompt("what is your name")
+console.log("Name : " + studentName)
+studentAge = prompt("what is your age")
+console.log("Age : " + studentAge)
+studentCity = prompt("what is your city")
+console.log("City : " + studentCity)
+
+//Task 6
+
+let course = "javaScript"
+alert(" Welcome to " + course)
+
+//Task 7
+
+ document.writeln("Hello student" + "<br>" + "Welcome to javaScript")
+
+
+//Task 8
+
+favouriteFood = "Biryani"
+favouriteColor = "Black"
+favouriteMovie = "Leo"
+console.log(" food : " + favouriteFood)
+console.log(" color : " + favouriteColor)
+console.log(" Movie : " + favouriteMovie)
+
+//Task 9
+
+ name = "Tarak"
+ age = "26"
+ phone = "7989825831"
+  email = "tarak@example.com"
+console.log("Name : " + name)
+console.log("Age : " + age) 
+console.log("Phone : " + phone)
+console.log("Email : " + email)
+
+// Task 10
+
+name = "Hello Tharak ," + "Welcome to MERN Stack Course"
+alert(name)
